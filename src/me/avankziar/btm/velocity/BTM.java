115300a55dcd4d5cc6df364824d39de8906353d6
“Heddo” <admin@heddo.eu>
2025-06-22 09:36:56 +0200

    Bump version to 7-9-0, Removed @Plugin of velocity/BTM, add ${version} to plugin.yml and bungee.yml, bump boosted-yaml to 1.3.7 and relocate to right package

--- src/me/avankziar/btm/velocity/BTM.java
+++ src/me/avankziar/btm/velocity/BTM.java
@@ -8,8 +8,6 @@ import java.util.logging.Logger;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
-import com.velocitypowered.api.plugin.Dependency;
-import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.PluginDescription;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
@@ -21,15 +19,6 @@ import me.avankziar.btm.general.database.YamlManager;
 import me.avankziar.btm.general.objecthandler.StaticValues;
 import me.avankziar.btm.velocity.listener.PluginMessageListener;
 
-@Plugin(id = "bungeeteleportmanager",
-		name = "BungeeTeleportManager", 
-		version = "7-5-2",
-		url = "https://www.spigotmc.org/resources/bungeeteleportmanager.80677/",
-		dependencies = {
-				@Dependency(id = "interfacehub", optional = false)
-		},
-		description = "A Teleport plugin with Back, Deathback, EntityTransport, Homes, SavePoints, RTP, TPA, TP and Warps",
-		authors = {"Avankziar"})
 public class BTM
 {
 	private static BTM plugin;
